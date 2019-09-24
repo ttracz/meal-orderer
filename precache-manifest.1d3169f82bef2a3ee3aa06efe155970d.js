819387b9381c0fb6757ba1c3e788e4195bfa9716
tymkowy <tymekjw@gmail.com>
2019-09-24 11:13:06 +0200

    Updates

---
+++ precache-manifest.1d3169f82bef2a3ee3aa06efe155970d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eefea4924d1b4b142278aaa377bd803",
+    "url": "/meal-orderer/index.html"
+  },
+  {
+    "revision": "48b43db05dfd03748cec",
+    "url": "/meal-orderer/static/css/2.d062aa4c.chunk.css"
+  },
+  {
+    "revision": "bc8e21777210a0679913",
+    "url": "/meal-orderer/static/css/main.1df75e5f.chunk.css"
+  },
+  {
+    "revision": "48b43db05dfd03748cec",
+    "url": "/meal-orderer/static/js/2.29d8d037.chunk.js"
+  },
+  {
+    "revision": "bc8e21777210a0679913",
+    "url": "/meal-orderer/static/js/main.3d409265.chunk.js"
+  },
+  {
+    "revision": "e9003e9747578a7e2863",
+    "url": "/meal-orderer/static/js/runtime~main.31ce2417.js"
+  }
+]);
